9ddd3ce848364d787ab8b05dcd65e919292b81d7
Joey-Perry <75853847+Joey-Perry@users.noreply.github.com>
2021-10-21 20:18:25 -0500

    Authentication update (#7)
    * Added initial getUser and login functions to authController
    
    * Working on updating logic to compare passwords in registered users
    
    * Fixed login issues by removing compareSync for now

---
+++ server/controllers/authController.js
@@ -0,0 +1,49 @@
+const bcrypt = require('bcryptjs');
+const db = (req) => req.app.get('db');
+
+const getUser = async (req, res) => {
+    // console.log(req.body);
+    try {
+        const user = await db(req).get_user(req.body.username);
+        // console.log(user);
+        return res.status(200).send(user);
+    } catch (err) {
+        console.log(`Error retrieving user: ${err}`);
+    }
+};
+
+const login = async (req, res) => {
+    const { username, password } = req.body;
+    
+    console.log(password);
+    
+    try {
+        const foundUser = await db(req).get_user(username);
+        const user = foundUser[0];
+        console.log(user);
+        if (!user){
+            return res.status(401).send('User nout found. Please register a new user before logging in.');
+        } else {
+            // const isAuthenticated = bcrypt.compareSync(password, user.password);
+            if (password !== user.password){
+                return res.status(403).send('Incorrect password!')
+            } else {
+                req.session.user = {
+                    id: user.id,
+                    username: user.username,
+                    email: user.email,
+                    firstName: user.first_name,
+                    lastName: user.last_name
+                }
+                console.log(req.session.user);
+                return res.status(200).send(req.session.user);
+            }
+        }
+    } catch (err) {
+        console.log(`Error logging in user: ${err}`);
+    }
+}
+
+module.exports = {
+    getUser, login
+}
