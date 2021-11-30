bd5ba6083634bf3d3159f40c28fc0eb75dda85da
Joey-Perry <Joeyap84@gmail.com>
2021-11-30 14:33:20 -0600

    Added confirmation page to signup component

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -12,6 +12,7 @@ const Signup = (props) => {
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [showConfirmationScreen, setShowConfirmationScreen] = useState(false);
 
     const register = () => {
         
@@ -22,56 +23,69 @@ const Signup = (props) => {
         axios.post('/auth/register', userInfo )
             .then(res => {
                 console.log(res.data);
-                const newUsername = res.data.username;
-                const loginInfo = { newUsername, password };
-                props.loginUser(loginInfo);
+                // const newUsername = res.data.username;
+                // const loginInfo = { newUsername, password };
+                // props.loginUser(loginInfo);
+                setShowConfirmationScreen(true);
             })
             .catch(err => console.log(err))
     }
     
     return (
         <section className='authentication'>
-        <h1>Register for Balance App</h1>
+            {!showConfirmationScreen && <> 
 
-        <section className='signin'>
+                    <h1>Register for Balance App</h1>
 
-        <h2>Create your account</h2>
+                    <section className='signin'>
 
-        <section className='login firstname'>
-            <h5>First name</h5>
-            <input value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
-        </section>
+                    <h2>Create your account</h2>
 
-        <section className='login lastname'>
-            <h5>Last name</h5>
-            <input value={lastName} onChange={(e) => setLastName(e.target.value)}/>
-        </section>
+                    <section className='login firstname'>
+                        <h5>First name</h5>
+                        <input value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
+                    </section>
 
-        <section className='login username'>
-        <h5>Username</h5>
-        <input value={username} onChange={(e) => setUsername(e.target.value)}/>
-        </section>
+                    <section className='login lastname'>
+                        <h5>Last name</h5>
+                        <input value={lastName} onChange={(e) => setLastName(e.target.value)}/>
+                    </section>
 
-        <section className='login email'>
-        <h5>Email</h5>
-        <input value={email} onChange={(e) => setEmail(e.target.value)}/>
-        </section>
+                    <section className='login username'>
+                    <h5>Username</h5>
+                    <input value={username} onChange={(e) => setUsername(e.target.value)}/>
+                    </section>
 
-        <section className='login password'>
-        <h5>Password</h5>
-        <input value={password} type='password' onChange={(e) => setPassword(e.target.value)}/>
-        </section>
+                    <section className='login email'>
+                    <h5>Email</h5>
+                    <input value={email} onChange={(e) => setEmail(e.target.value)}/>
+                    </section>
 
-        <button className='btn' onClick={register}> Create my account </button>
+                    <section className='login password'>
+                    <h5>Password</h5>
+                    <input value={password} type='password' onChange={(e) => setPassword(e.target.value)}/>
+                    </section>
 
-        </section>
+                    <button className='btn' onClick={register}> Create my account </button>
 
-        <section className='register'>
-            <p>Already have an account? <Link to='/'>Sign In</Link></p>
-        </section>
+                    </section>
 
+                    <section className='register'>
+                        <p>Already have an account? <Link to='/'>Sign In</Link></p>
+                    </section>
+                    </>}
+
+            {showConfirmationScreen && <>
+            
+                <h1>Congratulations on registering!</h1>
+                <p> Please refresh the page to login!</p>
+            
+            </>
+            }
+            
         </section>
     )
+    
 }
 
 const mapStateToProps = (reduxState) => {
